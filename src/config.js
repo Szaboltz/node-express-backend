8e278035858206984c5296cfaaf960d0ae648ecf
Henrique Szabo <szabopessoal@gmail.com>
2024-02-15 20:58:14 -0300

    Testing

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const PORT = 3000
+const URL = "https://localhost:"
+
+export default {PORT, URL}
---
+++ src/db/user.js
@@ -0,0 +1,5 @@
+export const users = [
+  {id: 1, name: "Henrique", email: "szabopessoal@gmail.com"},
+  {id: 2, name: "José", email: "jose@gmail.com"},
+  {id: 3, name: "Maria", email: "maria@gmail.com"},
+]
